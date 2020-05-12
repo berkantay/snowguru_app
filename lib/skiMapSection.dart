afb94e02d4584b477db94f4ed82994c945d643c4
berkantay <berkant497@gmail.com>
2020-05-12 14:06:33 +0300

    Map gathered succesfully

--- lib/skiMapSection.dart
+++ lib/skiMapSection.dart
@@ -10,13 +10,13 @@ class SkiMapSection extends StatelessWidget {
     return Expanded(
       child: new FlutterMap(
         options: new MapOptions(
-          minZoom: 0.0,
-          center: LatLng(40.71, -74.00),
+          center: LatLng(40.58, 31.78),
+          minZoom: 10.0,
         ),
         layers: [
           new TileLayerOptions(
               urlTemplate:
-                  "https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png",
+                  "https://snowmap.fast-sfc.com/pistes/{z}/{x}/{y}.png", //change base_snow_map to pistes
               subdomains: ['a', 'b', 'c'])
         ],
       ),
