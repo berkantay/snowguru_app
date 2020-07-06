c361f1f8af7cc5293f0ee9199a4b18525dbddeee
berkantay <berkant497@gmail.com>
2020-07-06 19:09:07 +0300

    Floating button editted

--- lib/floatingMenu.dart
+++ lib/floatingMenu.dart
@@ -1,37 +1,43 @@
+import 'package:fab_circular_menu/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:unicorndial/unicorndial.dart';
-import 'package:weather_icons/weather_icons.dart';
 
 class FloatingMenu extends StatelessWidget {
   const FloatingMenu({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return UnicornDialer(
-      parentButtonBackground: Colors.blue[900],
-      orientation: UnicornOrientation.HORIZONTAL,
-      parentButton: Icon(Icons.menu),
-      childButtons: _getProfileMenu(),
+    return FabCircularMenu(
+      ringColor: Color(0xAA000056),
+      fabCloseColor: Color(0xBFEB565B),
+      fabOpenColor: Color(0xAA000056),
+      fabCloseIcon: Icon(
+        Icons.close,
+        color: Colors.white,
+      ),
+      fabMargin: EdgeInsets.all(20.0),
+      fabElevation: 10.0,
+      animationDuration: Duration(seconds: 1),
+      ringDiameter: 400.0,
+      children: <Widget>[
+        IconButton(
+            icon: Icon(Icons.ac_unit),
+            color: Colors.white,
+            onPressed: () {
+              print("first icon");
+            }),
+        IconButton(
+            icon: Icon(Icons.ac_unit),
+            color: Colors.white,
+            onPressed: () {
+              print("second icon");
+            }),
+        IconButton(
+            icon: Icon(Icons.ac_unit),
+            color: Colors.white,
+            onPressed: () {
+              print("third icon");
+            })
+      ],
     );
   }
-}
-
-List<UnicornButton> _getProfileMenu() {
-  List<UnicornButton> children = [];
-
-  // Add Children here
-  children.add(_profileOption(iconData: Icons.list, onPressed: () {}));
-  children.add(_profileOption(iconData: Icons.play_arrow, onPressed: () {}));
-
-  return children;
-}
-
-Widget _profileOption({IconData iconData, Function onPressed}) {
-  return UnicornButton(
-      currentButton: FloatingActionButton(
-    backgroundColor: Colors.blue[700],
-    mini: true,
-    child: Icon(iconData),
-    onPressed: onPressed(),
-  ));
 }
